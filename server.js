a95b928f367b470ee59eef76f64b3f9a43602f6b
Javier Varela <jvarela@strands.com>
2020-11-14 00:45:50 +0100

    enable cors

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 const express = require("express");
 const app = express();
 
+//load cors
+const cors = require('cors');
+app.use(cors());
+
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
